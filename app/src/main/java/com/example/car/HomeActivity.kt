986d06dfc97a64f6550a474696ad8d7665b68a24
zainulfakhri16 <60900120013@uin-alauddin.ac.id>
2022-11-05 13:41:57 +0800

    navbar

--- app/src/main/java/com/example/car/HomeActivity.kt
+++ app/src/main/java/com/example/car/HomeActivity.kt
@@ -2,10 +2,39 @@ package com.example.car
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var navigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        init()
+        navigationListener()
+    }
+
+    private fun init() {
+        navigation = findViewById(R.id.bt_nav_layout)
+    }
+
+    private fun navigationListener() {
+        navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_fav -> {
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_sel -> {
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_profil -> {
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
